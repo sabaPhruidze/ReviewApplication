70b50dca02247622543931a001c20d674b6d5a5f
Saba Phruidze <sabaphruidze01@gmail.com>
2023-12-31 17:20:45 +0400

    I started creating screens folder where I have free components which I will use in the future

---
+++ screens/Home.tsx
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
---
+++ screens/ReviewDetails.tsx
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const ReviewDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Review Details</Text>
+    </View>
+  );
+};
+
+export default ReviewDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
