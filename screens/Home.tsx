6753bd6040260e2951c6bb73b0ad7a32c48a25b5
Saba Phruidze <sabaphruidze01@gmail.com>
2023-12-31 19:23:18 +0400

    I am going to use useState in order to pass the information

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,26 @@
 import {View, Text, StyleSheet, Button} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {GlobalStyles} from '../styles/Global';
 const Home = ({navigation}: {navigation: any}) => {
   const pressHandler = () => {
     navigation.navigate('ReviewDetails');
     // navigation.push('ReviewDetails');
   };
-
+  const [reviews, setReviews] = useState([
+    {
+      title: 'Zelda, Breath of Fresh Air',
+      rating: 5,
+      body: 'lorem ipsum',
+      key: '1',
+    },
+    {
+      title: 'Gotta Catch Them All (again)',
+      rating: 4,
+      body: 'lorem ipsum',
+      key: '2',
+    },
+    {title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
+  ]);
   return (
     <View style={GlobalStyles.container}>
       <Text style={GlobalStyles.text}>Home Screen</Text>
